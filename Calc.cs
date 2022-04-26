ab4c9ef9cc244c991da13cba08e51156b5a93bf9
Victor <vicktorteixeira.7@gmail.com>
2022-04-26 12:45:43 -0300

    hypotenuse calculator exercise

---
+++ Calc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hypotenuse_calculator
+{
+    class Calc
+    {
+        static double FirstCalc(double a, double b)
+        {
+            Console.WriteLine("Please, input your first value");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please, input your second value");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            a = Math.Pow(a, 2);
+            b = Math.Pow(b, 2);
+            double c = Math.Sqrt(a + b);
+
+            return c;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using hypotenuse_calculator;
+
+namespace hypotenuse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator();
+
+            Console.ReadLine();
+        }
+
+        static double Calculator()
+        {
+            Console.WriteLine("Please, input your first value");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please, input your second value");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            a = Math.Pow(a, 2);
+            b = Math.Pow(b, 2);
+            double c = Math.Sqrt(a + b);
+
+            Console.WriteLine($"\nO valor da hipotenusa é: {Math.Round(c, 5)}.");
+            return c;
+        }
+    }
+}
